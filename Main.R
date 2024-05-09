cbd2563894c56d766dd5640ca234c57771f73acd
meska <mporobic10@gmail.com>
2024-05-09 23:02:59 +0200

    Prepare project
    add dataset
    dataset description in mainscript
    etc

---
+++ Main.R
@@ -0,0 +1,11 @@
+#Dataset: data weekly earnings.csv
+#Assume that this cross-sectional dataset is a random iid (independent and identically distributed)
+#sample of 50.000 individuals from a single country, and that is was sampled in a recent year.
+#It contains the following variables:
+# • wage: individual’s weekly earnings
+# • educ: years of education
+# • educ parent: years of education of parents
+# • born: date of birth
+# • state: state the individual lives in
+# • iq: result of a recent IQ test
+# • dist to unitown: distance to the next city/town that offers higher education
